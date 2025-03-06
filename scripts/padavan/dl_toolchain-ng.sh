64124e536bab5789c4b8369a9e2a87244530c9b7
h8820 <101133120+h8820@users.noreply.github.com>
2025-03-06 12:18:36 +0800

    Update dl_toolchain-ng.sh

--- scripts/padavan/dl_toolchain-ng.sh
+++ scripts/padavan/dl_toolchain-ng.sh
@@ -5,8 +5,8 @@
 
 # File info
 DIR_NAME="out"
-FILE_NAME="padavan-ng-tlc"
-FILE_URL="https://github.com/vietanhbui2000/Build-Padavan-OpenWrt/raw/main/scripts/padavan/$FILE_NAME.tar.xz"
+FILE_NAME="out"
+FILE_URL="https://github.com/h8820/test1/raw/refs/heads/main/scripts/padavan/$FILE_NAME.tar.xz"
 
 # Download and extract file(s)
 curl -OL $FILE_URL
